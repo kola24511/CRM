537c8953f3428691e48dd3eddc56ba0fa4cc2bc7
kola24511 <kolyanemovdev@gmail.com>
2022-07-02 23:37:39 +0400

    add routing and index page

--- server/main.go
+++ server/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("server init")
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", index)
+	log.Fatal(router.Run(":8080"))
+}
+
+func index(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		"title": "Main website",
+	})
 }
